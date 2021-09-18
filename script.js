9293fa8767466678e435ac7572ac067a15d38a16
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-18 10:06:06 -0500

    added class to show ending screen when game is won

--- script.js
+++ script.js
@@ -30,26 +30,45 @@ const playGame = (function () {
         return theArray
     }
     function checkWinner () {
+        let winningScreen = document.querySelector('#end')
         if (theArray[0] == theArray[1] && theArray[1] == theArray[2] && theArray[0] !== "") {
             alert('horizontal win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
+            
+            
         } else if (theArray[3] == theArray[4] && theArray[4] == theArray[5] && theArray[3]  !== "") {
             alert('horizontal win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         } else if (theArray[6] == theArray[7] && theArray[7] == theArray[8] && theArray[6] !== "") {
             alert('horizontal win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         }
         //vertical
         if (theArray[0] == theArray[3] && theArray[3] == theArray[6] && theArray[0] !== "") {
             alert('vertical win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         } else if (theArray[1] == theArray[4] && theArray[4] == theArray[7] && theArray[1] !== "") {
             alert('vertical win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         } else if (theArray[2] == theArray[5] && theArray[5] == theArray[8] && theArray[2] !== "") {
             alert('vertical win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         }
         //diagonal
         if (theArray[0] == theArray[4] && theArray[4] == theArray[8] && theArray[0] !== "") {
             alert('diagonal win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         } else if (theArray[2] == theArray[4] && theArray[4] == theArray[6] && theArray[2] !== "") {
             alert ('diagonal win')
+            winningScreen.classList.remove('endScreen')
+            winningScreen.classList.add('showEndScreen')
         }
     }
 })()
