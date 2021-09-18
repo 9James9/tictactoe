b21b9e08566a39fb847797ec029a7df9aa7e9a42
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-18 06:04:47 -0500

    added option to choose team X or Y and display that choice on each click

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const gameBoard = (function () {
-    
+    chooseTeam = () => {
+        const chooseX = document.querySelector('#chooseX')
+        const chooseO = document.querySelector('#chooseO')
+        chooseX.addEventListener('click', () => {
+            console.log('X')
+            return team = 'X'  
+        })
+        chooseO.addEventListener('click', () => {
+            console.log("O")
+             return team = "O"
+        })
+    }
     squares = () => {
+        
         let tiles = ["","","","","","","","",""]
-        let team = 'O'
         const eachSquare = document.querySelectorAll('.boardSquare')
         let gameBoard = Array.from(eachSquare)
         for (let i = 0; i < gameBoard.length; i++) {
             gameBoard[i].addEventListener('click', () => {
                 console.log(i)
+                gameBoard[i].textContent = team
                 tiles.splice(i, 1 , team)
                 console.log(tiles)
             })
         }
-        gameBoard.forEach(square => {
-            square.addEventListener('click', () => {
-                square.textContent = 'O'
-                
-            })
-        })
+        
     checkIndex = (event) => {
         console.log(Array.from(event).indexOf(event,target))
     }
@@ -29,7 +36,7 @@ const gameBoard = (function () {
             
         })
         */
-        return tiles
+        return tiles, chooseTeam()
     }
     
     })()
