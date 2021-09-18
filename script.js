cefefe311f8f76020920305f525fbab9b17e508d
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-18 11:21:21 -0500

    added draws

--- script.js
+++ script.js
@@ -25,84 +25,67 @@ const playGame = (function () {
         for (let i = 0; i < boardArray.length; i++) {
             if (boardArray[i].textContent !== "")
             theArray.splice(i,1,boardArray[i].textContent)
-        
         }
         return theArray
     }
     function checkWinner () {
         let winningScreen = document.querySelector('#end')
         if (theArray[0] == theArray[1] && theArray[1] == theArray[2] && theArray[0] !== "") {
-            //alert('horizontal win')
-            //console.log(currentPlayer)
             assignWinner()
-            
             winningScreen.classList.add('showEndScreen')
-            
-            
+            return
         } else if (theArray[3] == theArray[4] && theArray[4] == theArray[5] && theArray[3]  !== "") {
-            //alert('horizontal win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
-            
+            return
         } else if (theArray[6] == theArray[7] && theArray[7] == theArray[8] && theArray[6] !== "") {
-            alert('horizontal win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
         }
         //vertical
         if (theArray[0] == theArray[3] && theArray[3] == theArray[6] && theArray[0] !== "") {
-            alert('vertical win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
         } else if (theArray[1] == theArray[4] && theArray[4] == theArray[7] && theArray[1] !== "") {
-            alert('vertical win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
         } else if (theArray[2] == theArray[5] && theArray[5] == theArray[8] && theArray[2] !== "") {
-            alert('vertical win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
         }
         //diagonal
         if (theArray[0] == theArray[4] && theArray[4] == theArray[8] && theArray[0] !== "") {
-            alert('diagonal win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
         } else if (theArray[2] == theArray[4] && theArray[4] == theArray[6] && theArray[2] !== "") {
-            alert ('diagonal win')
-         
             winningScreen.classList.add('showEndScreen')
             assignWinner()
+            return
+        } 
+        if (theArray[0] && theArray[1] && theArray[2] && theArray[3] && theArray[4] &&theArray[5] && theArray[6] && theArray[7] && theArray[8] !== "") {
+            
+            winningScreen.classList.add('showEndScreen')
+             draw = true
+             return assignWinner(draw)
         }
-
     }
-    function assignWinner () {
+    function assignWinner (draw) {
+        if (draw) {
+             return alert(`It's a draw.`)
+        }
         if (circleTurn == true) {
             winner = "X"
         } else winner = "Circle"
         alert(`The winner is ${winner}`)
     }
     function startOver () {
-        /*let winningScreen = document.querySelector('#end')
-            theArray = ["","","","","","","","",""]
-            winningScreen.classList.remove('showEndScreen')
-            let boardArray = Array.from(cellElements)
-            for (let i = 0; i < boardArray.length; i++) {
-                if (boardArray[i].textContent !== "") {
-                boardArray[i].textContent = ""
-                }
-            } 
-            return theArray
-            */
            window.location.reload()
     }
-    
     let resetBtn = document.querySelector('.restart')
     resetBtn.addEventListener('click', startOver)
 })()
