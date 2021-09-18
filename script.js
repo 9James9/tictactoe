ce3088d95fa1285050b9ba6d1575237dde58afd9
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-18 05:36:09 -0500

    converted nodelist to gameBoard array

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-const gameBoard = {
-    tiles: ["","","","","","","","",""],
-    squares() {
+const gameBoard = (function () {
+    
+    squares = () => {
+        let tiles = ["","","","","","","","",""]
         const eachSquare = document.querySelectorAll('.boardSquare')
+        let gameBoard = Array.from(eachSquare)
+        gameBoard.forEach(square => {
+            square.addEventListener('click', () => {
+                square.textContent = 'O'
+            })
+        })
+    
+        
+        /*eachSquare.forEach(square => {
+            document.addEventListener('click', () => {
+                square.textContent = 'O'
+            } )
+            
+        })
+        */
     }
-    }
-    gameBoard.squares()
+    })()
+squares()
