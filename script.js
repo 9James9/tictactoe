cbc1e2215b21b8e854e1ff05065330748065f1f2
James Driscoll <thejamesdriscoll@gmail.com>
2021-09-18 05:49:20 -0500

    each click puts an O in the correct place in the gameBoard array

--- script.js
+++ script.js
@@ -2,14 +2,25 @@ const gameBoard = (function () {
     
     squares = () => {
         let tiles = ["","","","","","","","",""]
+        let team = 'O'
         const eachSquare = document.querySelectorAll('.boardSquare')
         let gameBoard = Array.from(eachSquare)
+        for (let i = 0; i < gameBoard.length; i++) {
+            gameBoard[i].addEventListener('click', () => {
+                console.log(i)
+                tiles.splice(i, 1 , team)
+                console.log(tiles)
+            })
+        }
         gameBoard.forEach(square => {
             square.addEventListener('click', () => {
                 square.textContent = 'O'
+                
             })
         })
-    
+    checkIndex = (event) => {
+        console.log(Array.from(event).indexOf(event,target))
+    }
         
         /*eachSquare.forEach(square => {
             document.addEventListener('click', () => {
@@ -18,6 +29,8 @@ const gameBoard = (function () {
             
         })
         */
+        return tiles
     }
+    
     })()
 squares()
